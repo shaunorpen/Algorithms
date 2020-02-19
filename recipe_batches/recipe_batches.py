1e72616d4d44def0fff8b5a60065d4bcdee33249
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-02-19 13:59:20 +0000

    recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,9 +2,16 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
-
+def recipe_batches(recipe, store_cupboard):
+  whole_batches = 0
+  while True:
+    for ingredient in list(recipe.keys()):
+      if ingredient in store_cupboard and store_cupboard[ingredient] >= recipe[ingredient]:
+        store_cupboard[ingredient] -= recipe[ingredient]
+      else:
+        return whole_batches
+    whole_batches += 1
+  return whole_batches
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
