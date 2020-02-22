5d54f92ae2aaf8b4d7303b18b31fef415693183d
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-02-22 14:06:59 +0000

    Another solution

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,18 +2,36 @@
 
 import sys
 
-def making_change(amount, coins, currentCoin = 0):
+# def making_change(amount, coins, currentCoin = 0):
+#   if amount == 0:
+#     return 1
+#   if amount < 0:
+#     return 0
+#   nCombos = 0
+#   for coin in range(currentCoin, len(coins)):
+#     nCombos += making_change(amount - coins[coin], coins, coin)
+#   return nCombos
+
+# def making_change(amount, denominations):
+#   cache = {key: 0 for key in range(amount + 1)}
+#   cache[0] = 1
+
+#   for coin in denominations:
+#     for val in range (coin, amount + 1):
+#       cache[val] += cache[val - coin]
+
+#   return cache[amount]
+
+def helper(amount, denominations):
+  if amount < 0 or denominations == []:
+    return 0
   if amount == 0:
     return 1
-  if amount < 0:
-    return 0
-  nCombos = 0
-  for coin in range(currentCoin, len(coins)):
-    nCombos += making_change(amount - coins[coin], coins, coin)
-  return nCombos
+  return helper(amount - denominations[0], denominations) + helper(amount, denominations[1:])
 
-print(making_change(20, [1, 5, 10, 25, 50]))  
-
+def making_change(amount, denominations):
+  denominations.sort(reverse=True)
+  return helper(amount, denominations) 
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
